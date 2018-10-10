8b88b711c1e5ca7b7098e9b4496dea1e61074474
Jason Jackson <jrjackso@gmail.com>
2018-10-09 21:18:27 -0400

    Fix inverseVoteWeight formula
    The current formula uses sin from 0 to PI/2. This
    results in a faster increase in vote weight than the
    whitepaper depicted. This change uses sin from
    -PI/2 to PI/2 giving a slower vote weight ramp up
    with a lower amountVotedProducers. Using sin in this
    region matches more closely with the whitepaper picture.

--- contracts/eosio.system/voting.cpp
+++ contracts/eosio.system/voting.cpp
@@ -18,7 +18,6 @@
 #include <algorithm>
 #include <cmath>
 
-#define VOTE_VARIATION 0.001
 #define TWELVE_HOURS_US 43200000000
 #define SIX_MINUTES_US 360000000 // debug version
 #define TWELVE_MINUTES_US 720000000
@@ -238,9 +237,9 @@ namespace eosiosystem {
         return 0;
      }
 
-     double k = 1 - VOTE_VARIATION;
-     
-     return (k * sin(M_PI_2 * (amountVotedProducers / totalProducers)) + VOTE_VARIATION) * double(staked);
+     double percentVoted = amountVotedProducers / totalProducers;
+     double voteWeight = (sin(M_PI * percentVoted - M_PI_2) + 1.0) / 2.0;
+     return (voteWeight * staked);
    }
 
 
