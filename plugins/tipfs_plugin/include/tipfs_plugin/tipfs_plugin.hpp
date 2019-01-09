7785e8da9caa6886765fef0d0b3a1a6cf5ec9e1d
craigbranscom <craig.branscom@gmail.com>
2019-01-08 16:23:08 -0800

    initial commit

---
+++ plugins/tipfs_plugin/include/tipfs_plugin/tipfs_plugin.hpp
@@ -0,0 +1,31 @@
+/**
+ *  @file
+ *  @copyright defined in telos/LICENSE.txt
+ */
+#pragma once
+#include <appbase/application.hpp>
+#include <eosio/chain_plugin/chain_plugin.hpp>
+
+namespace eosio {
+
+using namespace appbase;
+
+class tipfs_plugin : public appbase::plugin<template_plugin> {
+public:
+    APPBASE_PLUGIN_REQUIRES((chain_plugin))
+
+    tipfs_plugin();
+    virtual ~tipfs_plugin();
+    
+    APPBASE_PLUGIN_REQUIRES()
+    virtual void set_program_options(options_description&, options_description& cfg) override;
+    
+    void plugin_initialize(const variables_map& options);
+    void plugin_startup();
+    void plugin_shutdown();
+
+private:
+    std::unique_ptr<class template_plugin_impl> my;
+};
+
+}
